d5409ac081138ee2648365d51728bd28130df141
Arcel <git@github.com:Arcellv/SimpleEcommerce_website.git>
2023-07-14 15:15:16 +0300

    last commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 /*----------------typing animation---------------- */
 var typed = new Typed(".typing", {
-    strings:["","web Designer", "Web Developer", "Freelancer"],
+    strings:["","Web Designer", "Web Developer", "Freelancer"],
     typeSpeed: 100,
     BackSpeed:60,
     loop: true
