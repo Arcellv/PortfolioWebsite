d98c8f38724ca9f045a96f38ddf72cb2c5d58156
Arcel <git@github.com:Arcellv/SimpleEcommerce_website.git>
2023-07-14 15:07:40 +0300

    last updates

---
+++ scripts.js
@@ -0,0 +1,29 @@
+/*----------------typing animation---------------- */
+var typed = new Typed(".typing", {
+    strings:["","web Designer", "Web Developer", "Freelancer"],
+    typeSpeed: 100,
+    BackSpeed:60,
+    loop: true
+})
+
+// Function to handle menu item clicks
+function handleMenuItemClick(event) {
+    const menuItems = document.querySelectorAll(".nav li a");
+    menuItems.forEach((menuItem) => {
+      menuItem.classList.remove("active");
+    });
+    event.target.classList.add("active");
+  }
+  
+  // Set event listeners for menu items
+  const menuItems = document.querySelectorAll(".nav li a");
+  menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", handleMenuItemClick);
+  });
+
+  const styleSwitcherIcon = document.querySelector(".style-switcher-toggler i");
+
+  styleSwitcherIcon.addEventListener("click", () => {
+    styleSwitcherIcon.classList.toggle("fa-spin");
+  });
+  
